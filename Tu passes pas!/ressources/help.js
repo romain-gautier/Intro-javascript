a9777e0cc1d8924bd3c97b9fec89c7353b222d72
Romain Gautier <romain.photog@gmail.com>
2016-02-02 10:40:20 +0100

    Projet P+

---
+++ Tu passes pas!/ressources/help.js
@@ -0,0 +1,14 @@
+/*
+
+1. Récupérer la valeur d'un champ de formulaire
+document.forms["x"].elements["y"].value;
+"x" étant le nom donné à l'attribut name de la balise <form>
+"y" étant le nom donnée à l'attribut id de la balise <input>
+
+2. Pensez à bien découper vos étapes pour organiser votre travail confortablement
+
+3. Pensez à faire appel à la documentation pour utiliser les fonctions JS déjà disponibles
+Lien vers la documentation :
+https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference
+
+*/
