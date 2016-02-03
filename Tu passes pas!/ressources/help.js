164976fee373daac3aa3133c509340cbe591ad93
Romain Gautier <romain.photog@gmail.com>
2016-02-03 15:18:18 +0100

    vérification du formulaire

--- Tu passes pas!/ressources/help.js
+++ Tu passes pas!/ressources/help.js
@@ -1,18 +1,3 @@
-/*
-
-1. Récupérer la valeur d'un champ de formulaire
-document.forms["x"].elements["y"].value;
-"x" étant le nom donné à l'attribut name de la balise <form>
-"y" étant le nom donnée à l'attribut id de la balise <input>
-
-2. Pensez à bien découper vos étapes pour organiser votre travail confortablement
-
-3. Pensez à faire appel à la documentation pour utiliser les fonctions JS déjà disponibles
-Lien vers la documentation :
-https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference
-
-*/
-
 function checkForm() {
 
   var nomUser = document.forms["form"].elements["name"].value;
@@ -24,7 +9,7 @@ function checkForm() {
   var http = "http://";
 
 
-  // le champ non doit être compris entre 3 et 10 caractères et pas de que des nombres
+  // le champ nom doit être compris entre 3 et 10 caractères et pas de que des nombres
   if (Number(nomUser)) {
     alert("Le nom ne peut pas contenir que des chiffres")
   } else {
